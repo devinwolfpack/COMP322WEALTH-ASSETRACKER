81c6eac0a4087aa626260e228952a6141e0fc6ea
devinwolfpack <dpmcneill@aggies.ncat.edu>
2026-06-11 04:33:36 -0400

    Add express server and asset API routes
    Created an Express server with middleware, static file hosting, a GET route for assets, and a POST route for adding assets

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+
+
+const app= express();
+const PORT = 3000;
+
+let assets =[];
+
+app.use(express.json());
+
+app.use(function(req,res,next) {
+    console.log(reqmethod + " request to " + req.url);
+    next();
+
+});
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/api/assets", function(req, res) {
+    res.json(assets);
+});
+
+app.post("/api/assets", function(req,res) {
+    const newAsset = {
+        name: req.body.name,
+        category: req.body.category,
+        value: Number(req.body.value)
+    };
+    assets.push(newAsset);
+
+    res.json(newAsset);
+});
+
+app.listen(PORT, function() {
+    console.log("Server is running on http://localhost:" + PORT);
+});
